e75fdbdcc9d4f01274270c1c436143fb74c4119c
frankchi24 <frankchi@gmail.com>
2025-09-29 00:25:58 +0800

    edit minor typos

--- tobefrank/deploy.sh
+++ tobefrank/deploy.sh
@@ -3,21 +3,25 @@ set -e
 
 # 預設 commit 訊息是時間戳
 msg="update: $(date)"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
+[ $# -ge 1 ] && msg="$*"
 
 # 1. build 網站
+echo "=== Build Hugo ==="
 hugo
 
 # 2. commit + push 內容 repo
+echo "=== Push source repo ==="
 git add .
-git commit -m "$msg"
-git push origin master
+git commit -m "$msg" || echo "No changes to commit in source repo"
+# 自動偵測目前分支名稱
+branch=$(git rev-parse --abbrev-ref HEAD)
+git push origin "$branch"
 
 # 3. commit + push public repo
+echo "=== Push public repo ==="
 cd public
 git add .
-git commit -m "$msg"
-git push origin main
+git commit -m "$msg" || echo "No changes to commit in public repo"
+branch_pub=$(git rev-parse --abbrev-ref HEAD)
+git push origin "$branch_pub"
 cd ..
